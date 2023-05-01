69dc46412f2c458cb7c41f8008f72f348435aadc
Jayden <132095916+J-Mbatia@users.noreply.github.com>
2023-04-30 20:18:11 -0500

    Set title to Calculator

--- calculator.py
+++ calculator.py
@@ -3,14 +3,11 @@ from calculatorController import *
 def main():
     app = QApplication([])
     window = Controller()
+    window.setWindowTitle('Calculator')
     window.geometry()
     window.resize(False, False)
     window.show()
     app.exec_()
-
-
-
-
 
 if __name__ == '__main__':
     main()
